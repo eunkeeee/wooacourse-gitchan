7149ef4193aa428b02de7faf65d998c008a104e8
gitchan <eunkeeeee@gmail.com>
2023-06-15 19:25:38 +0900

    feat: spring 프로젝트 추가

---
+++ gitchan-spring/src/main/java/com/example/gitchanspring/GitchanSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitchanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitchanSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitchanSpringApplication.class, args);
+	}
+
+}
---
+++ gitchan-spring/src/test/java/com/example/gitchanspring/GitchanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitchanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitchanSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
