f28e46dcf9e58a0fec85798f6cec6e93b26323ee
gitchan <eunkeeeee@gmail.com>
2023-06-15 19:34:16 +0900

    feat(build.gradle): 의존 라이브러리 추가

--- gitchan-spring/src/main/java/com/example/gitchanspring/GitchanSpringApplication.java
+++ gitchan-spring/src/main/java/com/example/gitchanspring/GitchanSpringApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitchanSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitchanSpringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GitchanSpringApplication.class, args);
+    }
 }
