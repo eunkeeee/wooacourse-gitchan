078c361e9c5e1b217b02595b0f7cd0ab4b0ec32a
gitchan <eunkeeeee@gmail.com>
2023-06-16 14:14:03 +0900

    feat(ConnectionTest): HikariDataSource 사용

--- gitchan-spring/src/test/java/com/example/gitchanspring/jdbc/connection/ConnectionTest.java
+++ gitchan-spring/src/test/java/com/example/gitchanspring/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.example.gitchanspring.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,6 +28,19 @@ class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         final DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        final HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSource(final DataSource dataSource) throws SQLException {
