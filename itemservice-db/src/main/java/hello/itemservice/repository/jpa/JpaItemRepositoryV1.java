d7022b8ff72e06eaaff6273689bf436f63aa5c60
gitchan <eunkeeeee@gmail.com>
2023-06-22 14:24:25 +0900

    style: 클래스 이름 변경

--- itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV1.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV1.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 @Transactional
-public class JpaItemRepository implements ItemRepository {
+public class JpaItemRepositoryV1 implements ItemRepository {
 
     private final EntityManager em;
 
