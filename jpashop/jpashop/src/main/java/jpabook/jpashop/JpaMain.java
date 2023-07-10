b804dc2dc67756ccfffd828a9338ff441e81deea
gitchan <eunkeeeee@gmail.com>
2023-07-10 11:40:02 +0900

    feat: native sql query

--- jpashop/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -6,9 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -25,13 +22,11 @@ public class JpaMain {
             member1.setName("깃짱");
             em.persist(member1);
 
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "깃짱"));
-            List<Member> result = em.createQuery(cq).getResultList();
-
+            List<Member> result = em.createNativeQuery(
+                    "select MEMBER_ID, city, street, zipcode, name FROM member",
+                            Member.class
+                    )
+                    .getResultList();
             for (Member member : result) {
                 System.out.println("member = " + member);
             }
