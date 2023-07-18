f9af8c32f6e23954081ad04e2eac5a45ccf91fdb
gitchan <eunkeeeee@gmail.com>
2023-07-18 09:51:14 +0900

    setting: 실전! 스프링 부트와 JPA 활용1 프로젝트 세팅

---
+++ jpareal1/src/main/java/jpabook/jpareal1/Jpareal1Application.java
@@ -0,0 +1,13 @@
+package jpabook.jpareal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpareal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpareal1Application.class, args);
+	}
+
+}
---
+++ jpareal1/src/test/java/jpabook/jpareal1/Jpareal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpareal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpareal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
