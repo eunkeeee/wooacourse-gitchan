443b170dca5c8c25e2b719cf692357d575d9678c
gitchan <eunkeeeee@gmail.com>
2023-07-27 09:43:23 +0900

    feat: Member 저장, 조회

---
+++ jpareal1/src/test/java/jpabook/jpareal1/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpareal1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void find() {
+        // given, when
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
