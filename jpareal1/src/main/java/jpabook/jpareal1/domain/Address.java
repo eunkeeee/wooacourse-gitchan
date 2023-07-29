0515e08ff15da9b9906601def53a19c538c1e757
gitchan <eunkeeeee@gmail.com>
2023-07-29 16:06:45 +0900

    refactor: 값 타입 객체는 setter 제거

--- jpareal1/src/main/java/jpabook/jpareal1/domain/Address.java
+++ jpareal1/src/main/java/jpabook/jpareal1/domain/Address.java
@@ -1,13 +1,18 @@
 package jpabook.jpareal1.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+import static lombok.AccessLevel.PROTECTED;
+import static lombok.AccessLevel.PUBLIC;
+
 @Embeddable
 @Getter
-@Setter
+@AllArgsConstructor(access = PUBLIC)
+@NoArgsConstructor(access = PROTECTED)
 public class Address {
 
     private String city;
