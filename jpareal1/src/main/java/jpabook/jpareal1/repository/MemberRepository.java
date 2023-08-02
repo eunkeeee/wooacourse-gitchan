b47b42ade5e3563989c4a79118cdf5406dca53bb
gitchan <eunkeeeee@gmail.com>
2023-08-02 09:54:59 +0900

    feat: MemberRepository 생성

---
+++ jpareal1/src/main/java/jpabook/jpareal1/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpareal1.repository;
+
+import jpabook.jpareal1.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where n.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
